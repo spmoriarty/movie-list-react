726555a78da875fb5723b46fa8b3c5cb41636095
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-17 15:31:29 -0700

    building out plan

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { setState } from 'react';
+import MovieDisplay from './MovieDisplay.js';
+const [title, setMovieTitle] = setState('StarWars');
+const [director, setMovieDirector] = setState('George Lucas');
+const [year, setMovieYear] = setState('1979');
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <><header>
+      <p>This right here</p>
+    </header>
+    
+    <form className='movie-input'>
+      <label>Movie Title</label>
+      <input className='title'/>
+      <label>Movie Director</label>
+      <input className='director'/>
+      <label>Movie Year</label>
+      <input className='year'/>
+      <select >
+        <option value='1'>Yellow</option>
+        <option value='2'>Orange</option>
+        <option value='3'>Red</option>
+      </select>
+      <button>Add Movie</button>
+    </form>
+
+    <div className='display-movies' >
+      <MovieDisplay 
+        setMovieDirector={setMovieDirector}
+        setMovieTitle={setMovieTitle}
+        setMovieYear={setMovieYear}/>
+      <h3>{title}</h3>
+      <p>{year}</p>
+      <p>by {director}</p>
+        
+      
+
     </div>
+
+
+
+    </>
+
   );
 }
 
